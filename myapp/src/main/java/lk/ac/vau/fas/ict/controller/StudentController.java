e76709bfa15f93e6029a6239913c6772a08627d9
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2025-05-01 14:53:24 +0530

    creating controllers

---
+++ myapp/src/main/java/lk/ac/vau/fas/ict/controller/StudentController.java
@@ -0,0 +1,22 @@
+package lk.ac.vau.fas.ict.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.ac.vau.fas.ict.model.Student;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	Student Bob = new Student("2021IT01", "Bob Marely", 23, "IT", 3.2);
+	Student Alice = new Student("2021IT02", "Alice corner", 24, "IT", 3.6);
+	Student John = new Student("2021IT03", "John Parker", 23, "IT", 3.3);
+
+	public StudentController() {	
+		getMap().put(Alice.getRegNo(), Alice);
+		getMap().put(Bob.getRegNo(), Bob);
+		getMap().put(John.getRegNo(), John);
+	}
+
+}
+}
